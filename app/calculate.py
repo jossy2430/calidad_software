11931df7ebb3eeacbb7d07c9a4b480aed6d2916c
joselyn gallardo <jo.gallardo@duocuc.cl>
2025-08-27 18:19:45 -0400

    subir trabajo

---
+++ app/calculate.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+def calcular_hipotenusa(cateto1, cateto2):
+    hipotenusa = sqrt(cateto1 ** 2 + cateto2 ** 2)
+    return hipotenusa
+
+print("resultado es "+str(calcular_hipotenusa(3,4)))
---
+++ test_sample.py
@@ -0,0 +1,4 @@
+from app.calculate import calcular_hipotenusa
+
+def test_calcular_hipotenusa():
+    assert calcular_hipotenusa(3, 4) == 5
