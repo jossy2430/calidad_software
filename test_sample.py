aa1f0ef6879eb512c098219a080881175b9aa8e4
joselyn gallardo <jo.gallardo@duocuc.cl>
2025-08-29 17:57:44 -0400

    Se agrega un nuevo caso de prueba

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,4 @@
 from app.calculate import calcular_hipotenusa
 
 def test_calcular_hipotenusa():
-    assert calcular_hipotenusa(3, 4) == 5
+    assert calcular_hipotenusa(5, 6) ==  7.810249675906654
