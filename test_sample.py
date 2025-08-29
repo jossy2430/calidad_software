8ccd24fd6bfaec3682dd5f68167a9f7efb5c4923
joselyn gallardo <jo.gallardo@duocuc.cl>
2025-08-29 18:31:03 -0400

    actualizado 3

--- test_sample.py
+++ test_sample.py
@@ -3,3 +3,8 @@ from app.calculate import calcular_hipotenusa
 def test_calcular_hipotenusa():
     assert calcular_hipotenusa(-8, -6) ==  10.0
 
+def test_calcular_hipotenusa_caso1():
+    assert calcular_hipotenusa(3, 4) == 5.0
+
+def test_calcular_hipotenusa_caso2():
+    assert calcular_hipotenusa(8, 15) == 17.0
